bbaa5e55db507ccdefc91dad117c9387894bb659
Fernando <fernando.e.mejia.e@gmail.com>
2025-07-13 11:35:24 -0600

    Demo in operation

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -3,9 +3,9 @@ import { SurveyWidget } from "../../sdk/src";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <SurveyWidget
-        surveyId="2405bdce-7620-470a-9a82-ba16935fd071"
+        surveyId={import.meta.env.VITE_SURVEY_ID}
       />
     </div>
   );
